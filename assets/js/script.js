0ec53ecd838ef1e6202f83a4b47d26b0b1f7bc00
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-05-19 22:37:16 -0400

    assigned var number, specialChar, lowerCase, upperCase arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
 // Assignment code here
 
+// number arrays
+var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// special character arrays
+var specialChar = ['!', '%', '&', ',', '*', '+', '-', '.', '/', '<', '>', '?', '~'];
+// lowercase letters array
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+// uppercase letters array
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
