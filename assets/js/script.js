c8e2b2cd86f4434945ba6f9b64900ab2389dcaaf
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-05-20 20:15:36 -0400

    while loop that enforces parameters

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,8 @@
 
 // number arrays
 var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-// special character arrays
-var specialChar = ['!', '%', '&', ',', '*', '+', '-', '.', '/', '<', '>', '?', '~'];
+// symbols arrays
+var symbols = ['!', '%', '&', ',', '*', '+', '-', '.', '/', '<', '>', '?', '~'];
 // lowercase letters array
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 // uppercase letters array
@@ -25,13 +25,22 @@ var generatePassword = function () {
 
   // determine parameters of password
   // does the user want special characters in their password?
-  var getSpecialChar = confirm('Click OK to confirm that you want special characters.')
+  var getSymbol = confirm('Click OK to confirm that you want special characters.');
   // does the user want numbers in their password?
-  var getNumber = confirm('Click OK to confirm that you want numbers.')
+  var getNumber = confirm('Click OK to confirm that you want numbers.');
   // does the user want lowercase letters in their password?
-  var getLowercase = confirm('Click OK to confirm that you want lowercase letters.')
+  var getLowercase = confirm('Click OK to confirm that you want lowercase letters.');
   // does the user want uppercase letters in their password?
-  var getUppercase = confirm('Click OK to confirm that you want uppercase letters.')
+  var getUppercase = confirm('Click OK to confirm that you want uppercase letters.');
+
+  // loop until the answer is within the parameters
+  while (getUppercase === false && getLowercase === false && getSymbol === false && getNumber === false) {
+    alert('You must choose at least one parameter');
+    var getSymbol = confirm('Click OK to confirm that you want special characters.');
+    var getNumber = confirm('Click OK to confirm that you want numbers.');
+    var getLowercase = confirm('Click OK to confirm that you want lowercase letters.');
+    var getUppercase = confirm('Click OK to confirm that you want uppercase letters.');
+  }
 };
 
 // Get references to the #generate element
